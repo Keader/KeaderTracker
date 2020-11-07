5c4f620c060bd6f1079f654e856010d52ac38f6f
Keader <keader.android@gmail.com>
2020-11-07 18:34:24 -0300

    Basic Navigation implemented

--- app/src/main/java/dev/keader/correiostracker/MainActivity.kt
+++ app/src/main/java/dev/keader/correiostracker/MainActivity.kt
@@ -1,15 +1,16 @@
 package dev.keader.correiostracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import dev.keader.correiostracker.databinding.ActivityMainBinding
-import dev.keader.correiostracker.archived.ArchivedFragment
-import dev.keader.correiostracker.home.HomeFragment
+import dev.keader.correiostracker.home.HomeFragmentDirections
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,13 +18,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.plant(Timber.DebugTree())
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setUpNavigation()
+
+        binding.floatingActionButton.setOnClickListener { view ->
+            //binding.bottomNavigation.visibility = View.GONE
+            view.findNavController().navigate(HomeFragmentDirections.actionGlobalAddPacketFragment())
+        }
+
+
+        // example of how navigate
+        //navController.navigate(HomeFragmentDirections.actionHomeFragmentToTrackDetailFragment("1234"))
+
     }
 
     private fun setUpNavigation() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         NavigationUI.setupWithNavController(binding.bottomNavigation, navHostFragment.navController)
+        Navigation.setViewNavController(binding.root, navHostFragment.navController)
     }
 }
--- app/src/main/java/dev/keader/correiostracker/trackdetail/TrackDetailFragment.kt
+++ app/src/main/java/dev/keader/correiostracker/trackdetail/TrackDetailFragment.kt
@@ -6,14 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.navArgs
 import dev.keader.correiostracker.R
 import dev.keader.correiostracker.databinding.FragmentTrackDetailBinding
+import timber.log.Timber
 
 class TrackDetailFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTrackDetailBinding>(inflater, R.layout.fragment_track_detail, container, false)
+
+        // Recuperando args
+        val args by navArgs<TrackDetailFragmentArgs>()
+
+        Timber.i("Got code: ${args.trackCode}")
+
         return binding.root
     }
 
