501eb79afaa73baf0586de2d8abba0a2b090bf32
Keader <keader.android@gmail.com>
2020-12-05 19:03:32 -0300

    Alert when object gets posted

--- app/src/main/java/dev/keader/correiostracker/repository/TrackingRepository.kt
+++ app/src/main/java/dev/keader/correiostracker/repository/TrackingRepository.kt
@@ -71,8 +71,9 @@ class TrackingRepository @Inject constructor(private val database: TrackingDatab
                 try {
                     val updatedItem = Correios.getTrack(item.item.code).toItemWithTracks()
                     updatedItem.item.name = item.item.name
-                    updatedItem.item.isWaitingPost = item.item.isWaitingPost
                     if (updatedItem.tracks.size != item.tracks.size)
+                        notificationList.add(updatedItem)
+                    else if (item.item.isWaitingPost && !updatedItem.item.isWaitingPost) // object posted, yay
                         notificationList.add(updatedItem)
                     database.insertItemWithTracks(updatedItem)
                 } catch (e: IOException) {
